da8a5b595f79a31bae0ba6ef545c84a019684d93
cognivore <66186054+cognivore@users.noreply.github.com>
2024-03-26 11:01:45 +0000

    Custom matcher impl return lol

--- exercises/01_better_assertions/07_custom_matcher/src/lib.rs
+++ exercises/01_better_assertions/07_custom_matcher/src/lib.rs
@@ -1,8 +1,30 @@
-use googletest::matcher::Matcher;
+use googletest::description::Description;
+use googletest::matcher::{Matcher, MatcherResult};
 use http::StatusCode;
 
+struct CustomMatcher;
+
+impl Matcher for CustomMatcher {
+    type ActualT = StatusCode;
+
+    fn matches(&self, actual: &Self::ActualT) -> MatcherResult {
+        if *actual == StatusCode::MOVED_PERMANENTLY {
+            MatcherResult::Match
+        } else {
+            MatcherResult::NoMatch
+        }
+    }
+
+    fn describe(&self, matcher_result: MatcherResult) -> Description {
+        match matcher_result {
+            MatcherResult::Match => format!("is a redirection code").into(),
+            MatcherResult::NoMatch => format!("which isn't a redirection code").into(),
+        }
+    }
+}
+
 pub fn is_redirect() -> impl Matcher<ActualT = StatusCode> {
-    todo!()
+    CustomMatcher
 }
 
 #[cfg(test)]
