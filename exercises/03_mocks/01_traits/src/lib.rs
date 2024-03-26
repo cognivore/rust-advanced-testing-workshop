8e146f6484005771b7a92938bd9e2b403743aff1
cognivore <66186054+cognivore@users.noreply.github.com>
2024-03-26 12:19:12 +0000

    Traits for mocking

--- exercises/03_mocks/01_traits/src/lib.rs
+++ exercises/03_mocks/01_traits/src/lib.rs
@@ -1,4 +1,7 @@
-pub fn square(x: i32, logger: PrintlnLogger) -> i32 {
+pub fn square<A>(x: i32, logger: &A) -> i32
+where
+    A: Logger,
+{
     let y = x * x;
     logger.log(&format!("{}^2 == {}", x, y));
     y
@@ -12,14 +15,32 @@ impl PrintlnLogger {
     }
 }
 
+pub trait Logger {
+    fn log(&self, msg: &str);
+}
+
+impl Logger for PrintlnLogger {
+    fn log(&self, msg: &str) {
+        println!("{}", msg);
+    }
+}
+
+pub struct TestLogger;
+
+impl Logger for TestLogger {
+    fn log(&self, _msg: &str) {}
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::TestLogger;
+
     use super::square;
     use googletest::assert_that;
     use googletest::matchers::eq;
 
     #[test]
     fn square_works() {
-        assert_eq!(square(2, todo!()), 4);
+        assert_eq!(square(2, &TestLogger), 4);
     }
 }
